d6973a37dda34d2cb1efc7b79f4fa15d855f8115
Mingrui <mingrui.zhang@taichi.graphics>
2021-11-28 10:39:13 +0800

    update demo

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import numpy as np
 from particle_system import ParticleSystem
 from sph_base import WCSPHSolver
 
-ti.init(arch=ti.gpu, device_memory_GB=3, packed=True)
+ti.init(arch=ti.gpu, device_memory_GB=4, packed=True)
 
 if __name__ == "__main__":
     ps = ParticleSystem((512, 512))
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     #             material=1)
 
     ps.add_cube(lower_corner=[6, 2],
-                cube_size=[1.0, 1.0],
-                velocity=[-10.0, -0.0],
+                cube_size=[3.0, 5.0],
+                velocity=[-5.0, -10.0],
                 density=1000.0,
                 color=0xFFFFF0,
                 material=1)
 
-    ps.add_cube(lower_corner=[3, 2],
+    ps.add_cube(lower_corner=[3, 1],
                 cube_size=[2.0, 6.0],
                 velocity=[0.0, -20.0],
                 density=1000.0,
