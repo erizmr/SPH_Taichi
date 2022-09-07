e80f955cc8134bde93f2929c7b376e5ae401e7d3
mingrui <mingrui.zhang@taichi.graphics>
2022-09-07 22:52:45 +0800

    add a demo script

---
+++ demo_dragon.py
@@ -0,0 +1,156 @@
+import taichi as ti
+import numpy as np
+import trimesh as tm
+from particle_system import ParticleSystem
+from WCSPH import WCSPHSolver
+from IISPH import IISPHSolver
+
+# ti.init(arch=ti.cpu)
+
+# Use GPU for higher peformance if available
+ti.init(arch=ti.cuda, device_memory_fraction=0.5)
+
+
+if __name__ == "__main__":
+    domain_size = 3
+    dim = 3
+    substeps = 2
+    output_frames = False
+    solver_type = "WCSPH"
+    # solver_type = "IISPH"
+    ps = ParticleSystem((domain_size,)*dim, GGUI=True)
+
+    y_offset = 0.5
+
+    # mesh = tm.load("./data/Dragon_50k.obj")
+    mesh = tm.load("./data/bunny_sparse.obj")
+    mesh_scale = 5
+    mesh.apply_scale(mesh_scale)
+    offset = np.array([1.5, 0.0 + y_offset, 1.5])
+
+    voxelized_mesh = mesh.voxelized(pitch=ps.particle_diameter).fill()
+    voxelized_points_np = voxelized_mesh.points + offset
+    num_particles_obj = voxelized_points_np.shape[0]
+    voxelized_points = ti.Vector.field(3, ti.f32, num_particles_obj)
+    voxelized_points.from_numpy(voxelized_points_np)
+
+    print("Rigid body, num of particles: ", num_particles_obj)
+
+    ps.add_particles(num_particles_obj, 
+                     voxelized_points_np, # position
+                     0.0 * np.ones((num_particles_obj, 3)), # velocity
+                     10 * np.ones(num_particles_obj), # density
+                     np.zeros(num_particles_obj), # pressure
+                     np.array([2 for _ in range(num_particles_obj)], dtype=int), # material
+                     255 * np.ones((num_particles_obj, 3))) # color
+        
+    # Fluid -1 
+    ps.add_cube(lower_corner=[0.6, 0.1 + y_offset, 0.6],
+                cube_size=[0.6, 1.2, 0.6],
+                velocity=[0.0, -3.0, 0.0],
+                density=1000.0,
+                color=(50,100,200),
+                material=1)
+                
+    # Moving rigid body - 1 
+    # ps.add_cube(lower_corner=[1.2, 0.2, 1.2],
+    #             cube_size=[0.2, 0.2, 0.2],
+    #             velocity=[0.0, 0.0, 0.0],
+    #             density=100.0,
+    #             color=(255,255,255),
+    #             material=2)
+
+    # Bottom boundary
+    ps.add_cube(lower_corner=[0.0, 0.0 + y_offset, 0.0],
+                cube_size=[2.0, ps.particle_diameter-0.001, 2.0],
+                velocity=[0.0, 0.0, 0.0],
+                density=1000.0,
+                color=(255,255,255),
+                material=0)
+    
+    # left boundary
+    ps.add_cube(lower_corner=[0.0, 0.0 + y_offset, 0.0],
+                cube_size=[ps.particle_diameter, 1.0, 2.0],
+                velocity=[0.0, 0.0, 0.0],
+                density=1000.0,
+                color=(255,255,255),
+                material=0)
+    
+    # right boundary
+    ps.add_cube(lower_corner=[0.0, 0.0 + y_offset, 0.0],
+                cube_size=[2.0, 1.0, ps.particle_diameter],
+                velocity=[0.0, 0.0, 0.0],
+                density=1000.0,
+                color=(255,255,255),
+                material=0)
+    
+    # Forward
+    # ps.add_cube(lower_corner=[0.0, 0.0 + y_offset, 2.0],
+    #             cube_size=[2.0, 1.0, ps.particle_diameter-0.001],
+    #             velocity=[0.0, 0.0, 0.0],
+    #             density=1000.0,
+    #             color=(255,255,255),
+    #             material=0)
+    
+    # Back
+    ps.add_cube(lower_corner=[2.0, 0.0 + y_offset, 0.0],
+                cube_size=[ps.particle_diameter-0.001, 1.0, 2.0],
+                velocity=[0.0, 0.0, 0.0],
+                density=1000.0,
+                color=(255,255,255),
+                material=0)
+        
+    if solver_type == "WCSPH":
+        solver = WCSPHSolver(ps)
+    elif solver_type == "IISPH":
+        solver = IISPHSolver(ps)
+
+
+    window = ti.ui.Window('SPH', (1024, 1024), show_window = True, vsync=True)
+
+    scene = ti.ui.Scene()
+    camera = ti.ui.make_camera()
+    camera.position(0.5, 1.0, 2.0)
+    camera.up(0.0, 1.0, 0.0)
+    camera.lookat(0.5, 0.5, 0.5)
+    camera.fov(60)
+    scene.set_camera(camera)
+
+    canvas = window.get_canvas()
+    radius = 0.002
+    movement_speed = 0.02
+    background_color = (0, 0, 0)  # 0xFFFFFF
+    particle_color = (1, 1, 1)
+
+    cnt = 0
+    solver.initialize_solver()
+    while window.running:
+        for i in range(substeps):
+            solver.step()
+        ps.copy_to_vis_buffer()
+        if ps.dim == 2:
+            canvas.set_background_color(background_color)
+            canvas.circles(ps.x_vis_buffer, radius=ps.particle_radius / 5, color=particle_color)
+        elif ps.dim == 3:
+            # user controlling of camera
+            position_change = ti.Vector([0.0, 0.0, 0.0])
+            up = ti.Vector([0.0, 1.0, 0.0])
+            # move camera up and down
+            if window.is_pressed("e"):
+                position_change = up * movement_speed
+            if window.is_pressed("q"):
+                position_change = -up * movement_speed
+            camera.position(*(camera.curr_position + position_change))
+            camera.lookat(*(camera.curr_lookat + position_change))
+            camera.track_user_inputs(window, movement_speed=movement_speed, hold_key=ti.ui.LMB)
+            scene.set_camera(camera)
+
+            scene.point_light((2.0, 2.0, 2.0), color=(1.0, 1.0, 1.0))
+            scene.particles(ps.x_vis_buffer, radius=ps.particle_radius, per_vertex_color=ps.color_vis_buffer)
+            canvas.scene(scene)
+
+        if output_frames:
+            if cnt % 20 == 0:
+                window.write_image(f"img_output/{cnt:04}.png")
+        cnt += 1
+        window.show()
