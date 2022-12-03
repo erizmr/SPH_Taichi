de92a111a5eabf90b9b96154665d74f48c1ac0e0
chunleili <li_cl@foxmail.com>
2022-12-03 21:02:15 +0800

    test read_ply

--- tests/test_read_ply.py
+++ tests/test_read_ply.py
@@ -1,18 +1,23 @@
 import trimesh
 import numpy as np
 import time
-pts=[]
 
-def test():
-    for i in range(1,1000):
-        PlyPath = f"D:\CG\meshSequence\sphere_points\sphere_points.{i:}.ply"
-        print("Reading ", PlyPath)
-        mesh = trimesh.load(PlyPath)
+plys=[]
+def read_ply(ply_path_no_ext, start=1, stop=1000):
+    pts=[]
+    for i in range(start, stop):
+        ply_path = ply_path_no_ext + f".{i:}.ply"
+        print("Reading ", ply_path)
+        mesh = trimesh.load(ply_path)
         v = mesh.vertices
         # mesh.show()
         pts.append(np.array(v))
+    return pts
 
-test()
+
+ply_path = "D:\CG\meshSequence\sphere_points\sphere_points"
+plys = read_ply(ply_path, start=1, stop=1000)
+
 
 import taichi as ti
 
@@ -27,17 +32,17 @@ scene = ti.ui.Scene()
 camera = ti.ui.make_camera()
 camera.position(1,1,2)
 camera.lookat(0, 0, 0)
-frameNum = 0
+frame_num = 0
 while window.running:
-    frameNum += 1
+    frame_num += 1
     camera.track_user_inputs(window, movement_speed=0.03, hold_key=ti.ui.RMB)
     scene.set_camera(camera)
     scene.ambient_light((0.8, 0.8, 0.8))
     scene.point_light(pos=(0.5, 1.5, 1.5), color=(1, 1, 1))
 
-    particles_pos.from_numpy(pts[frameNum])
+    particles_pos.from_numpy(plys[frame_num])
 
-    scene.particles(particles_pos, color = (0.68, 0.26, 0.19), radius = 0.01)
+    scene.particles(particles_pos, color = (0.6, 0.26, 0.19), radius = 0.01)
     canvas.scene(scene)
 
     time.sleep(1.0/24)
