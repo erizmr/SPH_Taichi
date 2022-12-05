3f15ca63de06649e943e64ddc6afe0eb3573798b
chunleili <li_cl@foxmail.com>
2022-12-06 01:42:30 +0800

    bug fix nsearch hash

--- nsearch/nsearch_base.py
+++ nsearch/nsearch_base.py
@@ -4,23 +4,13 @@ from particle_system import ParticleSystem
 
 @ti.data_oriented
 class NSearchBase():
-    def __init__(self,ps:ParticleSystem):
+    def __init__(self,ps:ParticleSystem, support_radius:float, max_num_neighbors:int = 50) -> None:
         self.ps = ps
-        self.max_num_neighbors = 50
-
-        # Grid id for each particle
-        self.grid_ids = ti.field(int, shape=self.ps.particle_max_num)
-
-        # Grid related properties
+        self.support_radius = support_radius
+        self.max_num_neighbors = max_num_neighbors
         self.grid_size = self.ps.support_radius
         self.grid_num = np.ceil(self.ps.domain_size / self.grid_size).astype(int)
-        self.padding = self.grid_size
 
-        # Particle num of each grid
-        self.grid_particles_num = ti.field(int, shape=int(self.grid_num[0]*self.grid_num[1]*self.grid_num[2]))
-        self.particle_neighbors = ti.field(int, shape=((self.ps.num_particles,) + (self.max_num_neighbors,)))
-
-    
     @ti.func
     def pos_to_index(self, pos):
         return (pos / self.grid_size).cast(int)
@@ -35,21 +25,10 @@ class NSearchBase():
 
     @ti.kernel
     def update_grid_id(self):
-        for I in ti.grouped(self.grid_particles_num):
-            self.grid_particles_num[I] = 0
-        for I in ti.grouped(self.ps.x):
-            grid_index = self.get_flatten_grid_index(self.ps.x[I])
-            self.grid_ids[I] = grid_index
-            ti.atomic_add(self.grid_particles_num[grid_index], 1)
+        pass
 
     def initialize_particle_system(self):
-        self.update_grid_id()
-
-    @ti.func
-    def for_all_neighbors(self, p_i, task: ti.template(), ret: ti.template()):
-        center_cell = self.pos_to_index(self.ps.x[p_i])
-        for offset in ti.grouped(ti.ndrange(*((-1, 2),) * self.ps.dim)):
-            grid_index = self.flatten_grid_index(center_cell + offset)
-            for p_j in range(self.grid_particles_num[ti.max(0, grid_index-1)], self.grid_particles_num[grid_index]):
-                if p_i[0] != p_j and (self.ps.x[p_i] - self.ps.x[p_j]).norm() < self.ps.support_radius:
-                    task(p_i, p_j, ret)
+        pass
+    
+    def neighborhood_search(self):
+        pass
--- nsearch/nsearch_hash.py
+++ nsearch/nsearch_hash.py
@@ -8,11 +8,7 @@ class NeighborhoodSearchHash():
         self.support_radius = support_radius
         self.num_particles = positions.shape[0]
 
-        # # read positions from the test data input
-        # self.positions = ti.Vector.field(dim, float, self.num_particles)
-        # self.positions.from_numpy(pos)
-
-        # # nsearch parameters
+        # nsearch parameters
         self.cell_size = 2 * self.support_radius #2 is heuristic, you can tune it
         self.cell_recpr = 1.0 / self.cell_size
         self.neighbor_radius = self.support_radius * 1.05
@@ -41,34 +37,43 @@ class NeighborhoodSearchHash():
         return 0 <= c[0] and c[0] < self.grid_size[0] and 0 <= c[1] and c[
             1] < self.grid_size[1]
 
+    @ti.func
+    def is_unique(self, p_i, pj):
+        flag = True
+        for nb_i in range(self.particle_num_neighbors[p_i]):
+            if self.particle_neighbors[p_i, nb_i] == pj:
+                flag = False
+        return flag
+
     @ti.kernel
     def neighborhood_search(self):
         # clear neighbor lookup table
-        for I in ti.grouped(self.grid_particles_num):
-            self.grid_particles_num[I] = 0
+        for I in ti.grouped(self.grid_num_particles_hash):
+            self.grid_num_particles_hash[I] = 0
         for I in ti.grouped(self.particle_neighbors):
             self.particle_neighbors[I] = -1
 
         # update grid
         for p_i in self.positions:
-            cell = self.pos_to_index(self.positions[p_i])
+            cell = self.get_cell(self.positions[p_i])
             hash = self.cell2hash(cell)
-            offs = ti.atomic_add(self.grid_particles_num[hash], 1)
-            self.grid2particle[hash, offs] = p_i
+            offs = ti.atomic_add(self.grid_num_particles_hash[hash], 1)
+            self.grid2particles_hash[hash, offs] = p_i
         # find particle neighbors
         for p_i in self.positions:
             pos_i = self.positions[p_i]
-            cell = self.pos_to_index(pos_i)
+            cell = self.get_cell(pos_i)
             hash = self.cell2hash(cell)
             nb_i = 0
             for offs in ti.static(ti.grouped(ti.ndrange((-1, 2), (-1, 2),(-1, 2)))):
                 cell_to_check = cell + offs
                 hash_to_check = self.cell2hash(cell_to_check)
                 if self.is_in_grid(cell_to_check):
-                    for j in range(self.grid_particles_num[hash_to_check]):
-                        p_j = self.grid2particle[hash_to_check, j]
+                    for j in range(self.grid_num_particles_hash[hash_to_check]):
+                        p_j = self.grid2particles_hash[hash_to_check, j]
                         if nb_i < self.max_num_neighbors and p_j != p_i and (
                                 pos_i - self.positions[p_j]).norm() < self.neighbor_radius:
-                            self.particle_neighbors[p_i, nb_i] = p_j
-                            nb_i += 1
-            self.particle_num_neighbors[p_i] = nb_i
+                            if self.is_unique(p_i, p_j):
+                                self.particle_neighbors[p_i, nb_i] = p_j
+                                nb_i += 1
+                                self.particle_num_neighbors[p_i] = nb_i
